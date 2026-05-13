623a4aa5331651ec6ea3f76549d39443e045ca96
Karanpandeyjpeg <pandeykaran321@gmail.com>
2026-05-13 16:37:47 +0930

    Update difference.py

---
+++ difference.py
@@ -0,0 +1,7 @@
+class Difference:
+    def _init_(self, x, y, radius, diff_type):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.diff_type = diff_type
+        self.found = False
